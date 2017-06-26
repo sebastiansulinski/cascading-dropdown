3f2a0f19ba0d9edf16d76dc3f1173bf5b3da109b
Sebastian Sulinski <Sebastian@Sebastians-Mac-Pro.local>
2017-06-26 08:56:19 +0100

    Updated module definition block

--- src/jquery.cascading-drop-down.js
+++ src/jquery.cascading-drop-down.js
@@ -2,15 +2,15 @@
  * ssdCascadingDropDown jQuery plugin
  * Examples and documentation at: https://github.com/sebastiansulinski/cascading-dropdown
  * Copyright (c) 2016 Sebastian Sulinski
- * Version: 1.4.2 (29-JAN-2017)
+ * Version: 1.4.2 (26-JUN-2017)
  * Licensed under the MIT.
  * Requires: jQuery v1.9 or later
  */
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
+    if (typeof define === 'function' && define.amd && define.amd.jQuery) {
         // AMD. Register as an anonymous module unless amdModuleId is set
-        define(['jquery'], function (a0) {
-            return (factory(a0));
+        define(['jquery'], function($) {
+            return factory($, root);
         });
     } else if (typeof exports === 'object') {
         // Node. Does not work with strict CommonJS, but
@@ -18,7 +18,7 @@
         // like Node.
         module.exports = factory(require('jquery'));
     } else {
-        factory(root["jQuery"]);
+        return factory(root.jQuery || root.$, root);
     }
 }(this, function ($) {
 
